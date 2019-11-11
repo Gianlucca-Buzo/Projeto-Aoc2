9cbae00afe08a76ba13dc6690de0decd8c9204fb
Gianlucca Buzo <gdmbuzo@inf.ufpel.edu.br>
2019-11-11 17:56:49 -0300

    modificao

--- exemplo.c
+++ exemplo.c
@@ -1,7 +1,23 @@
-//exemplo codigo c
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-int main{
-	int i;
+int main(int argc, char const *argv[]){
+  
+	int nsets, bsize, assoc, flag_saida,dado;
+	char *subs,*txt;
+	FILE *entrada;
+	entrada = fopen (txt,"r");  
+	nsets = atoi (argv[1]);
+	bsize = atoi (argv[2]);
+	assoc = atoi (argv[3]);
+	subs = argv [4];
+	flag_saida = atoi (argv [5]); 
+	txt = argv[6];
+
+	fscanf(entrada,"%d",&dado);
+	printf("%d",dado);
+  
+  	
 	return 0;
 }
