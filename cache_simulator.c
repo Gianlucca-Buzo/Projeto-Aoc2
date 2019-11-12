1a9dfcb831b0404ef1115bb3c4ece35a78917ab9
Gianlucca Buzo <gdmbuzo@inf.ufpel.edu.br>
2019-11-12 00:25:43 -0300

    Leitura de arquivo

--- cache_simulator.c
+++ cache_simulator.c
@@ -2,19 +2,35 @@
 #include <string.h>
 #include <stdlib.h>
 
+const int MAX = 1000;
+
 int main(int argc, char const *argv[]){
   
-	int nsets, bsize, assoc, flag_saida,dado;
-	char *subs,*txt; 
-	printf("Ola Mundo!\n");
+	int nsets, bsize, assoc, flag_saida,dados[MAX],j=0;
+	char *subs,*txt,file[8],teste; 
+	FILE *entrada;
+	//atribuições iniciais
 	nsets = atoi (argv[1]);
 	bsize = atoi (argv[2]);
 	assoc = atoi (argv[3]);
 	subs = argv [4];
 	flag_saida = atoi (argv [5]); 
 	txt = argv[6];
+	//copiando nome do arquivo para uma string
+	strcpy(file,txt);
+	file[8] = '\0';
+	entrada = fopen(file,"r");
+	
+	// leitura até o final do arquivo, menor que o máximo
+	while((fscanf(entrada,"%d",&dados[j]) != EOF) && j<MAX){
+		j++;
+	}
+
+	for (int i = 0; i < j; ++i)
+	{
+		printf("%d ",dados[i]);
+	}
   	
-  	printf("%d %d %d\n",nsets,bsize,assoc);
   	
 	return 0;
 }
