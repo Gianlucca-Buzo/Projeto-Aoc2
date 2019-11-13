041a4477a6b27bca3e0be4d07b99610d3d040276
Gianlucca Buzo <gdmbuzo@inf.ufpel.edu.br>
2019-11-13 12:21:53 -0300

    Atualizando .gitignore

--- exemplo.c
+++ exemplo.c
@@ -4,20 +4,24 @@
 
 int main(int argc, char const *argv[]){
   
-	int nsets, bsize, assoc, flag_saida,dado;
-	char *subs,*txt;
+	int nsets, bsize, assoc, flag_saida,dados[5];
+	char *subs,*txt; 
 	FILE *entrada;
-	entrada = fopen (txt,"r");  
+	entrada = fopen(txt,"r");
 	nsets = atoi (argv[1]);
 	bsize = atoi (argv[2]);
 	assoc = atoi (argv[3]);
 	subs = argv [4];
 	flag_saida = atoi (argv [5]); 
 	txt = argv[6];
-
-	fscanf(entrada,"%d",&dado);
-	printf("%d",dado);
-  
+  	for (int i = 0; i < 4; ++i)
+  	{
+  		fscanf(entrada,"%d",&dados[i]);
+  	}
+  	for (int i = 0; i < 4; ++i)
+  	{
+  		printf("%d ",dados[i]);
+  	}
   	
 	return 0;
 }
