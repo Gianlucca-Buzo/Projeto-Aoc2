e6b5bcb5f9e4be7cffb174187d7db3c2c2955d7d
Gianlucca Buzo <gdmbuzo@inf.ufpel.edu.br>
2019-11-13 12:00:22 -0300

    Mudando comentarios

--- cache_simulator.c
+++ cache_simulator.c
@@ -4,10 +4,19 @@
 
 const int MAX = 1000;
 
+/*
+nsets = numero de conjuntos
+bsize = tamanho do bloco
+assoc = nível de associatividade
+subs  = politica de substituição
+flag_ = flag de saida
+*/
+// teste branch
+
 int main(int argc, char const *argv[]){
   
-	int nsets, bsize, assoc, flag_saida,dados[MAX],j=0;
-	char *subs,*txt,file[8],teste; 
+	int nsets, bsize, assoc, flag_saida,dados[MAX],k=0,tag,i,j;
+	char *subs,file[15]; 
 	FILE *entrada;
 	//atribuições iniciais
 	nsets = atoi (argv[1]);
@@ -15,22 +24,22 @@ int main(int argc, char const *argv[]){
 	assoc = atoi (argv[3]);
 	subs = argv [4];
 	flag_saida = atoi (argv [5]); 
-	txt = argv[6];
 	//copiando nome do arquivo para uma string
-	strcpy(file,txt);
-	file[8] = '\0';
-	entrada = fopen(file,"r");
+	entrada = fopen(argv[6],"rb");
 	
 	// leitura até o final do arquivo, menor que o máximo
-	while((fscanf(entrada,"%d",&dados[j]) != EOF) && j<MAX){
-		j++;
-	}
 
-	for (int i = 0; i < j; ++i)
+	k = fread(&dados[0],sizeof(int),MAX,entrada);
+	i = nsets/assoc;
+	j = assoc;
+
+	int matriz[i][j];
+
+	for (int t = 0; t < k ; ++t)
 	{
-		printf("%d ",dados[i]);
+		tag = dados[t]%i;
+
 	}
-  	
   	
 	return 0;
 }
