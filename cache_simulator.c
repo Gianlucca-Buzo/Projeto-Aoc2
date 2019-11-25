b7de7e7bcff7fc5482df50cd3eab63993d51bcd7
Gianlucca Buzo <gdmbuzo@inf.ufpel.edu.br>
2019-11-25 15:20:38 -0300

    Recomeçando Trabalho

--- cache_simulator.c
+++ cache_simulator.c
@@ -3,11 +3,14 @@
 #include <stdlib.h>
 
 const int MAX = 1000;
+unsigned int lido;
+
+//int convertebinario();
 
 int main(int argc, char const *argv[]){
   
-	int nsets, bsize, assoc, flag_saida,dados[MAX],j=0;
-	char *subs,*txt,file[8],teste; 
+	int nsets, bsize, assoc, flag_saida,dados[MAX],j=0,tam=0;
+	char *subs,*txt,file[8],teste[32]; 
 	FILE *entrada;
 	//atribuições iniciais
 	nsets = atoi (argv[1]);
@@ -15,22 +18,57 @@ int main(int argc, char const *argv[]){
 	assoc = atoi (argv[3]);
 	subs = argv [4];
 	flag_saida = atoi (argv [5]); 
-	txt = argv[6];
-	//copiando nome do arquivo para uma string
-	strcpy(file,txt);
-	file[8] = '\0';
-	entrada = fopen(file,"r");
+	entrada = fopen(argv[6],"rb");
 	
-	// leitura até o final do arquivo, menor que o máximo
-	while((fscanf(entrada,"%d",&dados[j]) != EOF) && j<MAX){
-		j++;
-	}
+	if(entrada == NULL) {
+        printf("Arquivo inválido. \n");
+        return 1;
+    }
 
-	for (int i = 0; i < j; ++i)
-	{
-		printf("%d ",dados[i]);
-	}
-  	
-  	
-	return 0;
+    while (!feof(entrada)) {
+        fread(&lido, sizeof(unsigned int), 1, entrada);
+        dados[tam] = __builtin_bswap32(lido);
+        tam++;
+    }
+	tam--;
+
+	itoa(dados[0],teste,2);
+	printf("%d  %s",dados[0],teste);
+
+return 0;
 }
+/*
+int convertebinario(){
+	int dec,i,q[32],r[32]; //declaração das variáveis necessárias
+	i=0;                   //contador de elementos da array do loop while
+	printf("Digite o numero decimal que serah passado para binario:\n");
+	scanf("%d", &dec);
+	printf("%d em binarios: ",dec);
+		for (i = 0; i < 32 ; ++i)
+		{
+			r[i] = 0;
+		}
+	  //Algoritmo para o cálculo 
+		do{     // enquanto o quociente for diferente de 1, o programa continuará calculando
+	 		if((dec % 2)==0){
+	 			r[i] = 0;
+	 			dec = dec/2;
+	 		}
+	 			else{
+	 				r[i] = 1;
+	 				dec--;
+	 				dec = dec/2;
+	 			}
+	 			i++;
+	 	}while (dec >= 1);
+	 	i = 0;
+	 	int j=32;
+	 	while(i<32){
+	 		q[j] = r[i];
+	 	}
+
+
+	printf("\n");
+	return(0);
+}
+*/
