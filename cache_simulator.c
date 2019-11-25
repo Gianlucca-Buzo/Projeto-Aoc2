4222d2cbd847bf93904777d2db454c4c184acb07
Gianlucca Buzo <gdmbuzo@inf.ufpel.edu.br>
2019-11-25 15:25:15 -0300

    Recomeçando Trabalho 2

--- cache_simulator.c
+++ cache_simulator.c
@@ -5,12 +5,18 @@
 const int MAX = 1000;
 unsigned int lido;
 
+typedef struct dado {
+	char tag[100],indice[1000],offset[50];
+	bool validade;
+}dados;
+
 //int convertebinario();
 
 int main(int argc, char const *argv[]){
   
-	int nsets, bsize, assoc, flag_saida,dados[MAX],j=0,tam=0;
-	char *subs,*txt,file[8],teste[32]; 
+	int nsets, bsize, assoc, flag_saida,valores[MAX],j=0,tam=0;
+	dados dado[MAX];
+	char *subs,file[8],teste[32]; 
 	FILE *entrada;
 	//atribuições iniciais
 	nsets = atoi (argv[1]);
@@ -27,7 +33,7 @@ int main(int argc, char const *argv[]){
 
     while (!feof(entrada)) {
         fread(&lido, sizeof(unsigned int), 1, entrada);
-        dados[tam] = __builtin_bswap32(lido);
+        valores[tam] = __builtin_bswap32(lido);
         tam++;
     }
 	tam--;
@@ -37,38 +43,3 @@ int main(int argc, char const *argv[]){
 
 return 0;
 }
-/*
-int convertebinario(){
-	int dec,i,q[32],r[32]; //declaração das variáveis necessárias
-	i=0;                   //contador de elementos da array do loop while
-	printf("Digite o numero decimal que serah passado para binario:\n");
-	scanf("%d", &dec);
-	printf("%d em binarios: ",dec);
-		for (i = 0; i < 32 ; ++i)
-		{
-			r[i] = 0;
-		}
-	  //Algoritmo para o cálculo 
-		do{     // enquanto o quociente for diferente de 1, o programa continuará calculando
-	 		if((dec % 2)==0){
-	 			r[i] = 0;
-	 			dec = dec/2;
-	 		}
-	 			else{
-	 				r[i] = 1;
-	 				dec--;
-	 				dec = dec/2;
-	 			}
-	 			i++;
-	 	}while (dec >= 1);
-	 	i = 0;
-	 	int j=32;
-	 	while(i<32){
-	 		q[j] = r[i];
-	 	}
-
-
-	printf("\n");
-	return(0);
-}
-*/
